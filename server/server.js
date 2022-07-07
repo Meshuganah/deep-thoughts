6c79f93d496a726b665b222f25acf836dad15ac8
Josh Klein <kleinjjosh74@gmail.com>
2022-07-07 18:29:16 -0400

    added pre-built front end react code, and connected it to back end server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 //Import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -28,6 +29,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
   //Integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  //Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index/html'));
+  });
 
 
   db.once('open', () => {
